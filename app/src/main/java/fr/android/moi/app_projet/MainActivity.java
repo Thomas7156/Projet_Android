99f91f8ffd7342390e7a12b2685e823e32c2ded2
Tom Jouvet <tom.jouvet@live.fr>
2020-03-15 21:49:49 +0100

    Some fixes

--- app/src/main/java/fr/android/moi/app_projet/MainActivity.java
+++ app/src/main/java/fr/android/moi/app_projet/MainActivity.java
@@ -17,9 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_menu);
     }
 
     @Override
