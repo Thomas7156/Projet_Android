aafdf39a96075136fa526c2919e49eeb8de62208
Tom Jouvet <tom.jouvet@live.fr>
2020-03-28 13:10:06 +0100

    Add google maps service

--- app/src/main/java/fr/android/moi/app_projet/NewActivity.java
+++ app/src/main/java/fr/android/moi/app_projet/NewActivity.java
@@ -2,6 +2,7 @@ package fr.android.moi.app_projet;
 
 import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,40 +10,46 @@ import android.view.MenuItem;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-
-public class NewActivity extends AppCompatActivity {
-    private TextView latitude;
-    private TextView longitude;
+public class NewActivity extends FragmentActivity implements OnMapReadyCallback,LocationListener {
+    private GoogleMap mMap;
+    private String latitude;
+    private String longitude;
     private LocationManager locationManager;
     private String provider;
+    private TextView latEtLong;
+    private EditText player1, player2;
+    private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
-        //TextView
+        this.latEtLong = (TextView) findViewById(R.id.localisation_text);
+        this.player1 = (EditText) findViewById(R.id.text_player1);
+        this.player2 = (EditText) findViewById(R.id.text_player2);
         // Get the location manager
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         // Define the criteria how to select the location provider -> use
         // default
         Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
+        provider = locationManager.getBestProvider(criteria)
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
@@ -56,21 +63,38 @@ public class NewActivity extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+            System.out.println("requestPermissions OK");
             return;
         }
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        // Initialize the location fields
-        if (location != null) {
+        System.out.println("getLastKnowLocation");
+        locationManager.getLastKnownLocation(provider);// Initialize the location fields
+        if (this.location != null) {
             System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(location);
+            onLocationChanged(this.location);
+            System.out.println("location selected ");
         } else {
-            latitude.setText("Location not available");
-            longitude.setText("Location not available");
+            latitude = "lat not available";
+            longitude = "long not available";
+            latEtLong.setText("lat: "+latitude+", long : "+longitude);
+            System.out.println("Location not available onCreate");
         }
 
     }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
+        try {
+            //double lat = (double) (myLocation.getLatitude());
+            //double lng = (double) (myLocation.getLongitude());
+            //LatLng pos = new LatLng(lat, lng);
+            //mMap.addMarker(new MarkerOptions().position(pos).title("Marker in Paris"));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+        }
+        catch (Error e){
+            System.out.println(e.getMessage());
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -96,6 +120,7 @@ public class NewActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
@@ -109,9 +134,13 @@ public class NewActivity extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+            System.out.println("requestPermissions onResume");
             return;
         }
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
+        else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 150, this);
+            System.out.println("Locationsupdate onResume");
+        }
     }
 
     /* Remove the locationlistener updates when Activity is paused */
@@ -133,15 +162,16 @@ public class NewActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        locationManager.removeUpdates(this.getIntent());
+        locationManager.removeUpdates(this);
     }
 
     @Override
     public void onLocationChanged(Location location) {
         int lat = (int) (location.getLatitude());
         int lng = (int) (location.getLongitude());
-        latitude.setText(String.valueOf(lat));
-        longitude.setText(String.valueOf(lng));
+        this.latitude = String.valueOf(lat);
+        this.longitude = String.valueOf(lng);
+        latEtLong.setText("lat: "+latitude+", long : "+longitude);
     }
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -173,5 +203,8 @@ public class NewActivity extends AppCompatActivity {
         //GET INTENT EXTRA HERE
         startActivity(intent);
         return (true);
+    }
+    public boolean locate(View view)
+    {
     }
 }
