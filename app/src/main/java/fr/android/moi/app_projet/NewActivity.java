559722416d2399e63c61de660dc930a1d693a61d
Tom Jouvet <tom.jouvet@live.fr>
2020-04-07 15:39:05 +0200

    localisation done

--- app/src/main/java/fr/android/moi/app_projet/NewActivity.java
+++ app/src/main/java/fr/android/moi/app_projet/NewActivity.java
@@ -5,6 +5,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,8 +28,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class NewActivity extends FragmentActivity implements OnMapReadyCallback,LocationListener {
-    private GoogleMap mMap;
+public class NewActivity extends AppCompatActivity implements LocationListener {
     private String latitude;
     private String longitude;
     private LocationManager locationManager;
@@ -54,6 +54,7 @@ public class NewActivity extends FragmentActivity implements OnMapReadyCallback,
             requestPermissions(new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
                     Manifest.permission.INTERNET
             }, 10);
             // TODO: Consider calling
@@ -63,37 +64,33 @@ public class NewActivity extends FragmentActivity implements OnMapReadyCallback,
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            System.out.println("requestPermissions OK");
             return;
         }
-        System.out.println("getLastKnowLocation");
-        locationManager.getLastKnownLocation(provider);// Initialize the location fields
-        if (this.location != null) {
+        location = locationManager.getLastKnownLocation(provider);
+        System.out.println("onCreate");
+        System.out.println(provider);
+        System.out.println(locationManager.getLastKnownLocation(provider));
+        System.out.println("onCreate ended");
+        // Initialize the location fields
+        if (location != null) {
             System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(this.location);
+            onLocationChanged(location);
             System.out.println("location selected ");
         } else {
-            latitude = "lat not available";
-            longitude = "long not available";
-            latEtLong.setText("lat: "+latitude+", long : "+longitude);
+            latitude = null;
+            longitude = null;
+            latEtLong.setText("NEED A LOCALISATION");
             System.out.println("Location not available onCreate");
         }
 
     }
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        try {
-            //double lat = (double) (myLocation.getLatitude());
-            //double lng = (double) (myLocation.getLongitude());
-            //LatLng pos = new LatLng(lat, lng);
-            //mMap.addMarker(new MarkerOptions().position(pos).title("Marker in Paris"));
-            //mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
-        }
-        catch (Error e){
-            System.out.println(e.getMessage());
-        }
+    public void onLocationChanged(Location location_s) {
+        double lat = (double) (location_s.getLatitude());
+        double lng = (double) (location_s.getLongitude());
+        this.latitude = String.valueOf(lat);
+        this.longitude = String.valueOf(lng);
+        latEtLong.setText(latitude+","+longitude);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -121,8 +118,17 @@ public class NewActivity extends FragmentActivity implements OnMapReadyCallback,
     protected void onResume() {
         super.onResume();
 
+        this.latEtLong = (TextView) findViewById(R.id.localisation_text);
+        this.player1 = (EditText) findViewById(R.id.text_player1);
+        this.player2 = (EditText) findViewById(R.id.text_player2);
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria,false);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{
+            /*requestPermissions(new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.INTERNET
@@ -134,45 +140,14 @@ public class NewActivity extends FragmentActivity implements OnMapReadyCallback,
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            System.out.println("requestPermissions onResume");
+            System.out.println("requestPermissions onResume");*/
             return;
         }
-        else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 150, this);
-            System.out.println("Locationsupdate onResume");
-        }
+            locationManager.requestLocationUpdates(provider,400,1,(LocationListener) this);
+            System.out.println(provider);
     }
 
-    /* Remove the locationlistener updates when Activity is paused */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.INTERNET
-            }, 10);
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.removeUpdates(this);
-    }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        int lat = (int) (location.getLatitude());
-        int lng = (int) (location.getLongitude());
-        this.latitude = String.valueOf(lat);
-        this.longitude = String.valueOf(lng);
-        latEtLong.setText("lat: "+latitude+", long : "+longitude);
-    }
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // TODO Auto-generated method stub
@@ -199,20 +174,54 @@ public class NewActivity extends FragmentActivity implements OnMapReadyCallback,
     }
     public boolean start(View view)
     {
-        Intent intent = new Intent(this,RecordingActivity.class);
         //GET INTENT EXTRA HERE
 
-        String player_1_name = player1.getText().toString();
-        String player_2_name = player2.getText().toString();
-        String localisation = latEtLong.getText().toString();
-        intent.putExtra("localisation",localisation);
-        intent.putExtra("player1",player_1_name);
-        intent.putExtra("player2",player_2_name);
-        startActivity(intent);
-        return (true);
-    }
-    public boolean locate(View view)
-    {
+        if(player1.getText().toString().equals("") || player1.getText().toString().equals(null))
+        {
+            player1.setText("NEED A PLAYER 1");
+            System.out.println("ERROR 1");
+        }
+        if(player2.getText().toString().equals("") || player2.getText().toString().equals(null))
+        {
+            player2.setText("NEED A PLAYER 2");
+            System.out.println("ERROR 2");
+        }
+        if(latEtLong.getText().toString().equals("") || latEtLong.getText().toString().equals(null))
+        {
+            latEtLong.setText("NEED A LOCALISATION");
+            System.out.println("ERROR LOCALISATION");
+        }
+
+        System.out.println(player1.getText().toString());
+        System.out.println(player2.getText().toString());
+        System.out.println(latEtLong.getText().toString());
+
+
+        Log.e("player1",player1.getText().toString());
+        Log.e("player2",player2.getText().toString());
+        Log.e("localisation",latEtLong.getText().toString());
+        String player1_text=player1.getText().toString();
+        String player2_text=player2.getText().toString();
+        String localisation_text= latEtLong.getText().toString();
+
+        if(!player1_text.equals("") && !player1_text.equals("NEED A PLAYER 1") && !player2_text.equals("")&& !player2_text.equals("NEED A PLAYER 2") && !localisation_text.equals("NEED A LOCALISATION") && !localisation_text.equals("") ) {
+
+
+            Intent intent = new Intent(this,RecordingActivity.class);
+            String player_1_name = player1.getText().toString();
+            String player_2_name = player2.getText().toString();
+            String localisation = latEtLong.getText().toString();
+            intent.putExtra("localisation", localisation);
+            intent.putExtra("player1", player_1_name);
+            intent.putExtra("player2", player_2_name);
+            intent.putExtra("latitude", latitude);
+            intent.putExtra("longitude", longitude);
+            System.out.println("FAILED OK");
+            startActivity(intent);
+        }
+        else {
+            System.out.println("FAILED START");
+        }
         return (true);
     }
 }
