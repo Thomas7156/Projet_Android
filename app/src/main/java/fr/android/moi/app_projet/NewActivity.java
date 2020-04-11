c1b9fe982b89822e02ec65a8d0b97507136d0ce7
Tom Jouvet <tom.jouvet@live.fr>
2020-04-11 18:17:53 +0200

    Integrate Firebase + Pictures path, nedd to add to bdd

--- app/src/main/java/fr/android/moi/app_projet/NewActivity.java
+++ app/src/main/java/fr/android/moi/app_projet/NewActivity.java
@@ -20,12 +20,6 @@ import android.location.Criteria;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class NewActivity extends AppCompatActivity implements LocationListener {
--- app/src/main/java/fr/android/moi/app_projet/RecordingActivity.java
+++ app/src/main/java/fr/android/moi/app_projet/RecordingActivity.java
@@ -1,17 +1,22 @@
 package fr.android.moi.app_projet;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecordingActivity extends AppCompatActivity {
-    private static final int REQUEST_IMAGE_CAPTURE = 30;
-    private static final int GALLERY_REQUEST_CODE = 20;
+    private static final int TAKE_PICTURE = 1;
+    private static final int CHOOSE_PICTURE = 2;
+    private String photoLocation = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,17 +56,20 @@ public class RecordingActivity extends AppCompatActivity {
         //PHOTO FUNCTION
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, TAKE_PICTURE);
         }
         return (true);
     }
     public boolean picture_add(View view)
     {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
+        //Picture from gallery
+        Intent Gallerychoose = new Intent(Intent.ACTION_PICK);
+        Gallerychoose.setType("image/*");
         String[] mimeTypes = {"image/jpeg", "image/png"};
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+        //EXTRA
+        Gallerychoose.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        //STATT ACTIVITY
+        startActivityForResult(Gallerychoose, CHOOSE_PICTURE);
         return (true);
     }
     public boolean finish(View view)
@@ -70,5 +78,23 @@ public class RecordingActivity extends AppCompatActivity {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
         return (true);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case CHOOSE_PICTURE:
+                    //data.getData returns the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+                    photoLocation = selectedImage.toString();
+                    //addPicture
+                    break;
+
+                case TAKE_PICTURE:
+                    photoLocation = "camera";
+                    //addPicture
+                    break;
+            }
+        }
     }
 }
